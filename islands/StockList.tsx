ed3d8e0e7a7c25334be8cec54044c57e4a46d2d9
ryu <114303361+ryuapp@users.noreply.github.com>
2023-04-08 10:55:00 +0900

    Modify sort of stock list
    A to Z sort

--- islands/StockList.tsx
+++ islands/StockList.tsx
@@ -2,15 +2,22 @@ import { PageProps } from '$fresh/server.ts';
 
 export default function StockList({ data }: PageProps){
   const stockList = [];
-  for (const [i, product] of data.entries()) {
+  data.sort(function(a,b){
+    if( a["_sys"]["customOrder"] > b["_sys"]["customOrder"] ) return -1;
+    if( a["_sys"]["customOrder"] < b["_sys"]["customOrder"] ) return 1;
+    return 0;
+  })
+
+  for (const [i, stock] of data.reverse().entries()) {
     stockList.push(
     <div class="px-3 py-2 border w-60 col-span-1">
-      <div class="md:">{product["company_name"]}</div>
+      <div class="md:">{stock["company_name"]}</div>
       <div class="flex justify-between">
-        <div>{product["ticker_symbol"]}</div>
+        <div>{stock["ticker_symbol"]}</div>
         <div>CITY</div>
       </div>
     </div>);
   }
+  
   return <div class="mt-10 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 col-auto gap-2">{stockList}</div>
 }
