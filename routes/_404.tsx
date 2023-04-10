16368c5c6992db8d71b857a7a51e60e0a6624a97
ryu <114303361+ryuapp@users.noreply.github.com>
2023-04-10 23:06:34 +0900

    Add 404 page

---
+++ routes/_404.tsx
@@ -0,0 +1,16 @@
+import { UnknownPageProps } from '$fresh/server.ts'
+import Header from '../components/Header.tsx'
+
+export default function NotFoundPage({ url }: UnknownPageProps) {
+  return (
+    <>
+      <Header />
+      <div class='mt-10 mx-auto max-w-5xl'>
+        <main class='mx-5'>
+          <h1 class='text-3xl mb-5 font-bold'>404 NotFound</h1>
+          <p>ページが見つかりません。</p>
+        </main>
+      </div>
+    </>
+  )
+}
