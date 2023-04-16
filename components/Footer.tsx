e681eab21d58a34e59136f2e0cc826178a91c500
ryu <114303361+ryuapp@users.noreply.github.com>
2023-04-16 21:57:08 +0900

    Chore footer design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class='mx-auto mt-10 flex justify-center'>
+    <footer class='mx-auto mt-10 p-5 flex justify-center'>
       <p class='text-zinc-500 font-bold' href='/'>© 新潟開発</p>
     </footer>
   )
