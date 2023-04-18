e8b6935fdac013fc21b9796e7632e7714665bd2a
ryu <114303361+ryuapp@users.noreply.github.com>
2023-04-18 21:25:27 +0900

    Chore env name

--- utils/newt.ts
+++ utils/newt.ts
@@ -1,7 +1,7 @@
 import { Client, createClient } from 'newt-client-js'
 
 export const client: Client = createClient({
-  spaceUid: Deno.env.get('YOUR_SPACE_UID')!,
-  token: Deno.env.get('YOUR_CDN_API_TOKEN')!,
-  apiType: 'cdn', // "cdn" or "api"
+  spaceUid: Deno.env.get('NEWT_SPACE_UID') as string,
+  token: Deno.env.get('NEWT_API_TOKEN') as string,
+  apiType: Deno.env.get('NEWT_API_TYPE') as 'cdn' | 'api',
 })
