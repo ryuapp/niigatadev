53ec4dfbefe807263b9e04609e02b5ebea3da63f
ryu <114303361+ryuapp@users.noreply.github.com>
2023-04-19 08:51:49 +0900

    Chore semantic html

--- components/StockCard.tsx
+++ components/StockCard.tsx
@@ -5,12 +5,12 @@ interface StockProps {
 }
 export function StockCard({ name, ticker, city }: StockProps) {
   return (
-    <div class='px-3 py-2 border'>
+    <article class='px-3 py-2 border'>
       <div class='font-bold'>{name}</div>
       <div class='flex justify-between'>
         <div>{ticker}</div>
         <div class='text-gray-500'>{city}</div>
       </div>
-    </div>
+    </article>
   )
 }
